ea953ef89cdbde0aeeb2adf0cf996d59f244f401
Gustavo Schimidt <schmidtgustavo7@gmail.com>
2021-11-03 15:48:47 -0400

    Refatorando a aplicacao

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,13 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://admin:AyIjRVAr6Z3F2FLX@cluster0.stgjk.mongodb.net/smartRanking?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useCreateIndex: true,
-        useUnifiedTopology: true,
-        userFindAndModify: false,
-      },
+      'mongodb+srv://admin:AyIjRVAr6Z3F2FLX@cluster0.stgjk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+      // {
+      //   useNewUrlParser: true,
+      //   useCreateIndex: true,
+      //   useUnifiedTopology: true,
+      //   userFindAndModify: false,
+      // },
     ),
     JogadoresModule,
   ],
--- src/jogadores/jogadores.service.ts
+++ src/jogadores/jogadores.service.ts
@@ -1,35 +1,33 @@
-import { Injectable, Logger, NotFoundException } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador-dto';
 import { Jogador } from './interfaces/jogador.interface';
-import { v4 as uuid } from 'uuid';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 
 @Injectable()
 export class JogadoresService {
-  private jogadores: Jogador[] = [];
-
-  private readonly logger = new Logger(JogadoresService.name);
+  constructor(
+    @InjectModel('Jogador') private readonly jogadorModel: Model<Jogador>,
+  ) {}
 
   async criarAtualizarJogador(criarJogadorDto: CriarJogadorDto): Promise<void> {
     const { email } = criarJogadorDto;
 
-    const jogadorEncontrado = this.jogadores.find(
-      (jogador) => jogador.email === email,
-    );
+    const jogadorEncontrado = await this.jogadorModel.findOne({ email }).exec();
+
     if (jogadorEncontrado) {
-      this.atualizar(jogadorEncontrado, criarJogadorDto);
+      this.atualizar(criarJogadorDto);
     } else {
       this.criar(criarJogadorDto);
     }
   }
 
   async consultarTodosJogadores(): Promise<Jogador[]> {
-    return this.jogadores;
+    return await this.jogadorModel.find().exec();
   }
 
   async consultarJogadorPeloEmail(email: string): Promise<Jogador> {
-    const jogadorEncontrado = this.jogadores.find(
-      (jogador) => jogador.email === email,
-    );
+    const jogadorEncontrado = await this.jogadorModel.findOne({ email }).exec();
 
     if (!jogadorEncontrado) {
       throw new NotFoundException(`Jogador com email ${email} nao encontrado`);
@@ -38,38 +36,21 @@ export class JogadoresService {
     return jogadorEncontrado;
   }
 
-  async deletarJogador(email): Promise<void> {
-    const jogadorEncontrado = this.jogadores.find(
-      (jogador) => jogador.email === email,
-    );
-
-    this.jogadores = this.jogadores.filter(
-      (jogador) => jogador.email !== jogadorEncontrado.email,
-    );
+  async deletarJogador(email): Promise<any> {
+    return await this.jogadorModel.remove({ email }).exec();
   }
 
-  private criar(criarJogadorDto: CriarJogadorDto): void {
-    const { nome, telefoneCelular, email } = criarJogadorDto;
-
-    const jogador: Jogador = {
-      _id: uuid(),
-      nome,
-      telefoneCelular,
-      email,
-      ranking: 'A',
-      posicaoRanking: 1,
-      urlFotoJogador: 'www.google.com.br/foto123.jpg',
-    };
-    this.logger.log(`criarJogadorDto: ${JSON.stringify(jogador)}`);
-    this.jogadores.push(jogador);
+  private async criar(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+    const jogadorCriado = new this.jogadorModel(criarJogadorDto);
+    return await jogadorCriado.save();
   }
 
-  private atualizar(
-    jogadorEncontrado: Jogador,
-    criarJogadorDto: CriarJogadorDto,
-  ): void {
-    const { nome } = criarJogadorDto;
-
-    jogadorEncontrado.nome = nome;
+  private async atualizar(criarJogadorDto: CriarJogadorDto): Promise<Jogador> {
+    return await this.jogadorModel
+      .findOneAndUpdate(
+        { email: criarJogadorDto.email },
+        { $set: criarJogadorDto },
+      )
+      .exec();
   }
 }
